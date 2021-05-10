fa6c0f3cbd73603631e1ac67dc317b3e59e539a7
lnxd <48756329+lnxd@users.noreply.github.com>
2021-05-10 22:28:40 +1000

    Stripped whitespace from custom mode warning

--- custom-mine.sh
+++ custom-mine.sh
@@ -72,9 +72,9 @@ echo "Driver:  $INSTALLED_DRIVERV"
 echo "Target:  Unraid 6.9.0 - 6.9.2"
 echo
 echo "+---- WARNING ---- WARNING ---- WARNING ---- WARNING ---- WARNING ----"
-echo "|              You are running PhoenixMiner in custom mode!"
+echo "| You are running PhoenixMiner in custom mode!"
 echo "| Please make sure that you entered all necessary values correctly and"
-echo "|     that you have set a temperature limit for your graphics card!"
+echo "| that you have set a temperature limit for your graphics card!"
 echo "+---- WARNING ---- WARNING ---- WARNING ---- WARNING ---- WARNING ----"
 echo
 echo "Custom command:  $ADDITONAL"
